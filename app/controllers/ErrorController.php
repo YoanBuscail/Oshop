d8fe458e367167b59d00c78de19cd2e060bf7739
YoanBuscail <yoan.buscail@gmail.com>
2023-06-26 15:19:37 +0200

    structure

---
+++ app/controllers/ErrorController.php
@@ -0,0 +1,23 @@
+<?php
+
+class ErrorController
+{
+    public function error404()
+    {
+        $this->show("error404");
+    }
+
+    /**
+     * Affiche la page
+     *
+     * @param string $viewName
+     * @param array $viewData (Facultatif)
+     */
+    
+    private function show($viewName, $viewData = [])
+    {
+        require_once __DIR__ . "/../views/header.tpl.php";
+        require_once __DIR__ . "/../views/$viewName.tpl.php";
+        require_once __DIR__ . "/../views/footer.tpl.php";
+    }
+}
